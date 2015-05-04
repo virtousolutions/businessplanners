305ff7d9b313abcda0fa4658f3c8326de6d59aa6
Edford Bedia <fordbedia@gmail.com>
2015-05-05 01:28:42 +0800

    ....

--- main/app/views/app.blade.php
+++ main/app/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Business Planners</title>
+	<title>Business Plannerss</title>
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
