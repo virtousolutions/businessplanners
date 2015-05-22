c73f2885f21746e38146cc9b73d7fb7cd854b218
Mark <markjoymacaso@gmail.com>
2015-05-23 00:38:23 +0800

    JS change

--- assets/javascript/plan/details.js
+++ assets/javascript/plan/details.js
@@ -30,6 +30,65 @@ $(document).ready( function () {
                         message: 'You entered an invalid year.'
                     }
                 }
+            },
+            contact_name: {
+                validators: {
+                    notEmpty: {
+                        message: 'Contact name is a required field.'
+                    },
+                    stringLength: {
+                        min: 3,
+                        message: 'Contact name must be more than 3 characters long.'
+                    }
+                }
+            },
+	        address_1: {
+                validators: {
+                    notEmpty: {
+                        message: 'Address 1 is a required field.'
+                    },
+                }
+            },
+            city: {
+                validators: {
+                    notEmpty: {
+                        message: 'Town/City is a required field.'
+                    },
+                }
+            },
+            country: {
+                validators: {
+                    notEmpty: {
+                        message: 'Country is a required field.'
+                    },
+                }
+            },
+	        post_code: {
+                validators: {
+                    notEmpty: {
+                        message: 'Post code is a required field.'
+                    },
+		            zipCode: {
+                        message: 'The value is not a valid UK postcode.'
+                    }
+                }
+            },
+	        telephone: {
+                validators: {
+                    notEmpty: {
+                        message: 'Telephone number is a required field.'
+                    }
+                }
+            },
+	        email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Email address is a required field.'
+                    },
+                    emailAddress: {
+                        message: 'Email address is not valid.'
+                    }
+                }
             }
         }
     });
