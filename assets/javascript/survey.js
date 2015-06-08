ee14cad48c4b222e802c9923420bd7e7b12fceaf
Mark <markjoymacaso@gmail.com>
2015-06-08 23:42:33 +0800

    Changes

--- assets/javascript/survey.js
+++ assets/javascript/survey.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
     $('#survey-form').bootstrapValidator({
         fields: {
+            full_name : {
+                validators: {
+                    notEmpty: {
+                        message: 'Full Name is a required field.'
+                    },
+                    stringLength: {
+                        min: 3,
+                        message: 'Name must be more than 5 characters long.'
+                    }
+                }
+            },
+            email_address : {
+                validators: {
+                    notEmpty: {
+                        message: 'Email address is a required field.'
+                    },
+                    emailAddress: {
+                        message: 'Email address is not valid.'
+                    }
+                }
+            },
             'business_plan_purpose[]' : {
                 validators: {
                     notEmpty: {
